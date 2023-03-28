e10b460a680af696254c14f6278c2cad6a924187
Heber G de Paula <mando.sarko@gmail.com>
2023-03-28 09:23:36 -0300

    commit inicial

---
+++ Jogos.py
@@ -0,0 +1,22 @@
+import forca
+import advinhacao
+
+
+def escolha_jogo():
+    print("*********************************")
+    print("********Escolha seu jogo!********")
+    print("*********************************")
+
+    print("(1) Forca (2) Advinhação")
+
+    jogo = int(input("Qual o jogo?"))
+
+    if(jogo == 1):
+        print("Jogando Forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogando Advinhação!")
+        advinhacao.Jogar()
+
+if(__name__ == "__main__"):
+    escolha_jogo()
